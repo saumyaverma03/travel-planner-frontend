a35a4c22672647680a365c2889cc854f837e53c1
saumyaverma03 <saumyavvvvv16@gmail.com>
2026-02-23 19:17:53 +0530

    add landing page

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <h1>Welcome to the Travel Planner!</h1>
+        
+    </div>
+  )
+}
+
+export default LandingPage
