bc1c995df18925b4d4eacb877d9d23bc31058491
saumyaverma03 <saumyavvvvv16@gmail.com>
2026-03-03 16:45:32 +0530

    Merged remote main into local main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
-import Navbar from "./components/ui/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/nav" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
